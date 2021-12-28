f5cfee013383b3afd6fd3caf88874ec0ed2d832c
holyaustin <41966983+holyaustin@users.noreply.github.com>
2021-12-28 17:56:31 +0100

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+
+  export const nftlibraryaddress = "0xEbb3113e97eeaA16Dcc2FebD6a1617AB731066AE"
+  export const nftaddress = "0x5E4e5347eB417982375Ef9BDe0B77F4322FCF79F"
+  export const nftvideoaddress = "0x1B7a6536f23a16e198246A3f80Cd646f86856F11"
+  export const nftvaddress = "0x60cF847C6Ea49009ae290F749451F4CB66CAD0B2"
+  
---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+ },
+  reactStrictMode: true,
+  exportTrailingSlash: true,
+}
+
+
+
+
